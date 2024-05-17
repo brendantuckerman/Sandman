1caa82e403ae04f38fb9b633bff467418c07f433
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-05-17 17:11:21 +1000

    Basic config set up

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+  <h1> Hi there! </h1>
+  <h2> {{ $config.public.myValue || "No value yet "}}</h2>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,56 @@ export default defineNuxtConfig({
 
   alias: {
     "~": "./",  //Setting alias for root
+  },
+
+  analyzeDir: "./.nuxt/analyze", //Set alias for nuxt analyse
+
+  app: { //Defaults --see $dev, $prod etc below
+    head: {
+      title: "Slingshot CMS",
+      titleTemplate: '%s | @Newtrick',
+      "meta": [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        },
+        
+      ],
+      "link": [],
+      "style": [],
+      "script": [],
+      "noscript": [],
+    }
+  },
+
+  $development: { // This will be merged with the default above
+    debug: true, //Debug mode
+    app: {
+      head: {
+        title: "DEV",
+      }
+    }
+  }, 
+
+  $production: {
+    app: {
+      head: {
+        title: "PROD"
+
+      }
+    }
+  },
+
+  runtimeConfig: {
+    public: {
+      //https://www.youtube.com/watch?v=_FYV5WfiWvs
+      //the below is only run at build time, not
+      // run time so cant be changed
+      //myValue: process.env.OTHER_ENV_VAR
+    }
   }
+
 })
