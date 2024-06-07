227d7809205ac6dbd81d6b0474ee99fe6292856c
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-06-07 17:29:46 +1000

    Styling for search bar

--- components/input/InputSearch.vue
+++ components/input/InputSearch.vue
@@ -3,15 +3,23 @@
     <div class="input-text-container">
       <label class="flex flex-col dark:text-retro-white"> 
           {{  searchLabelText }}
-        <div >
+        <div class="flex relative">
             <input 
+              :placeholder="placeholder"
+              :name="name"
               type="Search"
               class="bg-retro-yellow dark:bg-retro-white p-2 
               border border-2 border-retro-green dark:border-retro-yellow"
-              :placeholder="placeholder"
-              :name="name"
+             
             />
-            <input type="submit">
+            <div class="flex items-center justify-center absolute right-2  search-icon top-2">
+            <input type="submit" 
+              class="absolute right-2 top-1 z-50 search-input w-8 text-transparent bg-transparent  hover:cursor-pointer" 
+              @click="submitSearch">
+              <font-awesome-icon :icon="['fas', 'magnifying-glass']" 
+              class="absolute right-2 top-1 pr-2"
+              />
+            </div>
         </div>
       </label>
     </div>
@@ -34,9 +42,23 @@ const props = defineProps({
     default: "Enter your search terms..."
   },
 
-  name: String,
-  default: "Search"
-  
+  name: {
+    type: String,
+    default: "Search"
+  }
 })
 
+const submitSearch = function(){
+    console.log('Search clicked');
+    //Search function goes here
+}
+
 </script>
+
+<style scoped>
+
+.search-input{
+  color: transparent;
+}
+
+</style>
